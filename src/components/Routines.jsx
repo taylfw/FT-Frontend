95596b43370218222742ad403210d41fe3ceeb39
Shaun <thedeadsent@gmail.com>
2021-11-08 19:25:38 -0500

    comp frames

---
+++ src/components/Routines.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+
+const Routines = () => {
+  return (
+    <header>
+      <h1>Welcome to the Routines Component</h1>
+    </header>
+  );
+};
+
+export default Routines;
